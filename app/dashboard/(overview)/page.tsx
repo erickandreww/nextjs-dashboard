45786dceb8e577c16638b874901f357e58b7f424
erickandreww <erick.andrew.moura@gmail.com>
2025-01-24 23:21:05 -0300

    "adjusting eslint error"

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from "../../ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
